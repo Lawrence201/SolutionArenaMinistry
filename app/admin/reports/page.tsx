a106e7d1890a64718df6349efb9a7c02d8c8c788
Lawrence Egyin <lawrenceantwi63@gmail.com>
2026-02-18 18:42:18 +0000

    Updated files with recent changes

--- app/admin/reports/page.tsx
+++ app/admin/reports/page.tsx
@@ -49,7 +49,7 @@ export default function ReportsPage() {
                             <polyline points="7 10 12 15 17 10" />
                             <line x1="12" y1="15" x2="12" y2="3" />
                         </svg>
-                        Export All
+                        Export Now
                     </button>
                 </div>
             </div>
